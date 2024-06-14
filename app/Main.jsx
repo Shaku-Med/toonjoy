eab0da39fa758fd54ed031b47198375b6ddb499e
Magendaam <amaramagenda18@gmail.com>
2024-06-14 07:38:10 -0400

    s

--- app/Main.jsx
+++ app/Main.jsx
@@ -128,10 +128,10 @@ const App = ({ k, dv, db, KeepQuiet }) => {
 
   return (
     <>
-      <link rel="preload" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" as="style"></link>
+      {/* <link rel="preload" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" as="style"></link>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossOrigin="anonymous"></link>
       
-      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css"></link>
+      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css"></link> */}
 
       <Connect.Provider value={{ KeepQuiet, audio, k, vl, setvl, db, dv, imgF, setimgF, CImg, owner, setowner, posts, setposts, selid, setselid }}>
         <AnimatePresence>
