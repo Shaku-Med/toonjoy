03ef8d40fa21b30817ce22552d1d0a69f83c590c
Magendaam <amaramagenda18@gmail.com>
2024-06-14 08:23:44 -0400

    s

--- app/Function/Objects.jsx
+++ app/Function/Objects.jsx
@@ -1,36 +1,33 @@
-import * as CryptoJS from 'crypto-js';
+import CryptoJS from 'crypto-js'
 import { enc, dec } from 'encmed';
-// interface ObjectsType {
-//     encDec(data: string, key: string, isdec: boolean, iscrypto: boolean): string | null;
-// }
 
-const Objects = {
+let Objects = {
     encDec(data, key, isdec, iscrypto) {
         try {
             if (isdec) {
-                let d = CryptoJS.AES.decrypt(data.includes('U2FsdGVkX1') ? data : `U2FsdGVkX1${data}`, key).toString(CryptoJS.enc.Utf8);
+                let d = CryptoJS.AES.decrypt(data.includes('U2FsdGVkX1') ? data : `U2FsdGVkX1${data}`, key).toString(CryptoJS.enc.Utf8)
                 if (d && d !== '') {
                     if (iscrypto) {
-                        return d;
+                        return d
+                    }
+                    else {
+                        let dd = dec(d, key)
+                    if (dd) {
+                        return dd
                     } else {
-                        let dd = dec(d, key);
-                        if (dd) {
-                            return dd;
-                        } else {
-                            return null;
-                        }
+                        return null
+                    }
                     }
                 } else {
-                    return null;
+                    return null
                 }
             } else {
-                return CryptoJS.AES.encrypt(iscrypto ? data : enc(data, key), key).toString().split(`U2FsdGVkX1`)[1];
+                return CryptoJS.AES.encrypt(iscrypto ? data : enc(data, key), key).toString().split(`U2FsdGVkX1`)[1]
             }
-        } catch (error) {
-            console.error('Encryption/Decryption error:', error);
-            return null;
+        } catch {
+            return null
         }
-    },
-};
+    }
+}
 
-export default Objects;
+export default Objects
