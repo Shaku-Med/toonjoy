4b1557403b0cdd4a29a6d7c887da4af4fecec85f
Medzy Ems Amara <100793520+Shaku-Med@users.noreply.github.com>
2024-08-05 03:01:12 -0400

    Update Nav.jsx

--- app/Functions/Nav/Nav.jsx
+++ app/Functions/Nav/Nav.jsx
@@ -6,7 +6,7 @@ let Nav = () => {
     return (
         <>
          <div className="ma_box min-h-[300px] relative h-full w-full">
-            <i className="bi bi-x-lg brd top-2 right-2 bg-danger shadow-lg w-10 h-10 flex items-center justify-center text-lg absolute z-[10000000000]" />
+{/*             <i className="bi bi-x-lg brd top-2 right-2 bg-danger shadow-lg w-10 h-10 flex items-center justify-center text-lg absolute z-[10000000000]" /> */}
          <iframe className=' w-full h-full min-h-full absolute top-0 left-0' src="https://backend.toonjoy.org/admin" frameborder="0" />
          </div>
         </>
