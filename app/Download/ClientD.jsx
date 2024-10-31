aefee56fcbbe142f9877be7f4d8664fe3673d6c6
Medzy Ems Amara <100793520+Shaku-Med@users.noreply.github.com>
2024-10-31 03:06:15 -0400

    Update ClientD.jsx

--- app/Download/ClientD.jsx
+++ app/Download/ClientD.jsx
@@ -593,7 +593,7 @@ const ClientD = () => {
                                                             
                                                         </div>
 
-                                                        <div className={`px-2 py-2 text-center line-clamp-2 dis`}>{new Date(parseInt(vl.time).toLocaleString())}</div>
+                                                        <div className={`px-2 py-2 text-center line-clamp-1 dis`}>{new Date(parseInt(vl.time).toLocaleTimeString())}</div>
                                                     </motion.div>
                                                 );
                                             })
